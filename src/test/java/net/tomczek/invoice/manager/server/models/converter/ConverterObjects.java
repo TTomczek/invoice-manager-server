c7ada369b07a80ef6a4c2fe5d1dc085c7bb8670c
Tim Tomczek <>
2024-11-20 13:51:13 +0100

    Added remaining ConverterObjects

--- src/test/java/net/tomczek/invoice/manager/server/models/converter/ConverterObjects.java
+++ src/test/java/net/tomczek/invoice/manager/server/models/converter/ConverterObjects.java
@@ -3,9 +3,11 @@ package net.tomczek.invoice.manager.server.models.converter;
 import net.tomczek.invoice.manager.api.server.model.*;
 import net.tomczek.invoice.manager.server.entities.*;
 import net.tomczek.invoice.manager.server.models.*;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.core.io.ByteArrayResource;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -79,8 +81,62 @@ public class ConverterObjects {
         fileDTO.setFileName("Rechnung.pdf");
         fileDTO.setData(new ByteArrayResource(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
 
-        invoice = new Invoice(3456, "Rechnung 1", true, businessPartner, fileWithContent, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        salesTax = new SalesTax(3456, "MwSt. 19%", new BigDecimal("19.00"));
+        salesTaxDAO = new SalesTaxDAO(3456, "MwSt. 19%", new BigDecimal("19.00"));
+        salesTaxDTO = new SalesTaxDTO();
+        salesTaxDTO.setId(3456);
+        salesTaxDTO.setName("MwSt. 19%");
+        salesTaxDTO.setRate(new BigDecimal("19.00"));
 
+        invoice = new Invoice(3456, "Rechnung 1", true, "PreText of invoice", "PostText of Invoice", LocalDate.now().minus(5, ChronoUnit.DAYS), LocalDate.now().minus(1, ChronoUnit.DAYS), "0987654", 123, salesTax, new ArrayList<>(), contactPerson, null, businessPartner);
+        invoiceDAO = new InvoiceDAO(3456, "Rechnung 1", true, "PreText of invoice", "PostText of Invoice", LocalDate.now().minus(5, ChronoUnit.DAYS), LocalDate.now().minus(1, ChronoUnit.DAYS), "0987654", 123, salesTaxDAO, new ArrayList<>(), contactPersonDAO, null, businessPartnerDAO);
+        invoiceDTO = new InvoiceDTO();
+        invoiceDTO.setId(3456);
+        invoiceDTO.setDescription("Rechnung 1");
+        invoiceDTO.setViaMail(true);
+        invoiceDTO.setPreText("PreText of invoice");
+        invoiceDTO.setPostText("PostText of Invoice");
+        invoiceDTO.setServiceFrom(LocalDate.now().minus(5, ChronoUnit.DAYS));
+        invoiceDTO.setServiceTo(LocalDate.now().minus(1, ChronoUnit.DAYS));
+        invoiceDTO.setOrderNumber("0987654");
+        invoiceDTO.setFile(123);
+        invoiceDTO.setSalesTax(salesTaxDTO.getId());
+        invoiceDTO.setPositions(new ArrayList<>());
+        invoiceDTO.setReceiver(contactPersonDTO.getId());
+        invoiceDTO.setInvoiceTemplate(null);
+        invoiceDTO.setCustomerNumber(businessPartnerDTO.getId());
 
+        businessPartner.setInvoices(Arrays.asList(invoice));
+        businessPartnerDAO.setInvoiceDAOS(Arrays.asList(invoiceDAO));
+        businessPartnerDTO.setInvoices(Arrays.asList(3456));
+
+        invoiceTemplate = new InvoiceTemplate(5476574, "Rechnungsvorlage 1", 4.0f, 2.0f, 2.0f, 2.0f, 45);
+        invoiceTemplateDAO = new InvoiceTemplateDAO(5476574, "Rechnungsvorlage 1", 4.0f, 2.0f, 2.0f, 2.0f, 45);
+        invoiceTemplateDTO = new InvoiceTemplateDTO();
+        invoiceTemplateDTO.setId(5476574);
+        invoiceTemplateDTO.setName("Rechnungsvorlage 1");
+        invoiceTemplateDTO.setMarginTopFirstPage(4.0f);
+        invoiceTemplateDTO.setMarginBottomFirstPage(2.0f);
+        invoiceTemplateDTO.setMarginTopOtherPages(2.0f);
+        invoiceTemplateDTO.setMarginBottomOtherPages(2.0f);
+        invoiceTemplateDTO.setFileId(45);
+
+        invoice.setInvoiceTemplate(invoiceTemplate);
+        invoiceDAO.setInvoiceTemplateDAO(invoiceTemplateDAO);
+        invoiceDTO.setInvoiceTemplate(invoiceTemplateDTO.getId());
+
+        invoicePosition = new InvoicePosition(789, "Position 1", 23, UnitET.PIECE, new BigDecimal(1000), invoice);
+        invoicePositionDAO = new InvoicePositionDAO(789, "Position 1", 23, UnitET.PIECE, new BigDecimal(1000), invoiceDAO);
+        invoicePositionDTO = new InvoicePositionDTO();
+        invoicePositionDTO.setId(789);
+        invoicePositionDTO.setDescription("Position 1");
+        invoicePositionDTO.setQuantity(23.0f);
+        invoicePositionDTO.setUnit(InvoicePositionDTO.UnitEnum.PIECE);
+        invoicePositionDTO.setPricePerUnitInCents(new BigDecimal(1000));
+        invoicePositionDTO.setInvoice(invoiceDTO.getId());
+
+        invoice.setInvoicePositions(Arrays.asList(invoicePosition));
+        invoiceDAO.setInvoicePosition(Arrays.asList(invoicePositionDAO));
+        invoiceDTO.setPositions(Arrays.asList(789));
     }
 }
