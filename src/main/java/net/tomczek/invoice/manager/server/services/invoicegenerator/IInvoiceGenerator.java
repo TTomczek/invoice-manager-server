8b4704523d92b9cf8d33f3f86228664eed67b8da
Tim Tomczek <>
2025-01-15 17:26:38 +0100

    reenabled context-path

--- src/main/java/net/tomczek/invoice/manager/server/services/invoicegenerator/IInvoiceGenerator.java
+++ src/main/java/net/tomczek/invoice/manager/server/services/invoicegenerator/IInvoiceGenerator.java
@@ -1,7 +1,6 @@
 package net.tomczek.invoice.manager.server.services.invoicegenerator;
 
 import net.tomczek.invoice.manager.server.entities.Invoice;
-import net.tomczek.invoice.manager.server.models.FileWithContent;
 
 public interface IInvoiceGenerator {
     byte[] generateInvoice(Invoice invoice);
